515ee37261f5ce1edf0975ad1d60301ef195aa9e
cordovabenito633 <cordovabenito633@gmail.com>
2026-01-18 15:23:13 -0500

    cambios en la clase aplicacion

--- ProjectoTienda/src/main/java/proyectoProgramacionI/Aplicacion.java
+++ ProjectoTienda/src/main/java/proyectoProgramacionI/Aplicacion.java
@@ -10,26 +10,25 @@ public class Aplicacion{
     ap=new Aplicacion();
     ArrayList<Producto> arr1;
     arr1=ap.ingresarProducto();
-    ap.Vendiendo(arr1);
+    ap.mostrarVoleta(arr1);
   }
 
-  boolean preguntarProductos(){
+  public boolean preguntarFin(){
     Scanner tec;
-    tec=new Scanner(System.in);
-    String pregunta;
+    String respuesta;
     boolean estado;
-    System.out.println("Desea Ingresar mas productos"+ " Si"+ " O" + " No");
-    pregunta=tec.nextLine();
-    if(pregunta=="Si"){
-      estado=true;
+    tec=new Scanner(System.in);
+    System.out.println("Desea ingresar mas productos (si o no)");
+    respuesta = new String(tec.nextLine());
+    if(respuesta.trim().toLowerCase() == "si"){
+      estado = true;
     }else{
-      estado=false;
+      estado = false;
     }
     return estado;
   }
 
-
-  ArrayList<Producto> ingresarProducto(){
+  public ArrayList<Producto> ingresarProducto(){
     Scanner tec;
     ProductoNacional p1;
     ProductoImportado p2;
@@ -37,73 +36,53 @@ public class Aplicacion{
     boolean estado;
     double precio,impuestos;
     int stock;
-    ArrayList<Producto> arr;
-    arr=new ArrayList<>();
-    tec=new Scanner(System.in);
+    ArrayList<Producto> productosComprados;
+
+    productosComprados = new ArrayList<>();
+    tec = new Scanner(System.in);
+
     do{
       System.out.println("Ingresa el tipo de producto Producto Nacional o Producto Importado");
-      tipoProducto=tec.nextLine();
+      tipoProducto = new String(tec.nextLine());
       System.out.println("Ingrese el nombre del producto");
-      nomProducto=tec.nextLine();
+      nomProducto = tec.nextLine();
       System.out.println("Ingrese la marca del producto");
-      marca=tec.nextLine();
+      marca = tec.nextLine();
       System.out.println("Ingrese el precio del producto");
-      precio=tec.nextDouble();
+      precio = tec.nextDouble();
       System.out.println("Ingrese la cantidad de productos que quedan");
-      stock=tec.nextInt();
-      System.out.println("Ingrese si el producto sigue disponible o no"+ " True o False");
-      estado=tec.nextBoolean();
-      if (tipoProducto=="Producto Importado") {
-        impuestos=tec.nextDouble();
-        p2=new ProductoImportado(precio, tipoProducto, marca, stock, estado, impuestos);
-        arr.add(p2);
-      }else{
-        p1=new ProductoNacional(precio,nomProducto,marca,stock,estado); 
-        arr.add(p1);
+      stock = tec.nextInt();
+      System.out.println("Ingrese si el producto sigue disponible o no (True o False)");
+      estado = tec.nextBoolean();
+      if (tipoProducto.trim().toLowerCase() == "producto importado") {
+        impuestos = tec.nextDouble();
+        p2 = new ProductoImportado(precio, tipoProducto, marca, stock, estado, impuestos);
+        productosComprados.add(p2);
+      } else {
+        p1 = new ProductoNacional(precio, nomProducto, marca, stock, estado); 
+        productosComprados.add(p1);
       }
-    }while(preguntarProductos()==true);
-    return arr;
+    } while(preguntarFin());
+    return productosComprados;
   }
 
-  ArrayList<ProductoImportado> ingresarProductoImportado(){
-    Scanner tec;
-    ProductoImportado p1;
-    String nomProducto,marca;
-    boolean estado;
-    double precio,impuesto;
-    int stock;
-    ArrayList<ProductoImportado> arr;
-    arr=new ArrayList<>();
-    tec=new Scanner(System.in);
-    do{
-      nomProducto=tec.nextLine();
-      marca=tec.nextLine();
-      precio=tec.nextDouble();
-      stock=tec.nextInt();
-      estado=tec.nextBoolean();
-      impuesto=tec.nextDouble();
-      p1=new ProductoImportado(precio,nomProducto,marca,stock,estado,impuesto); 
-      arr.add(p1);
-    }while(preguntarProductos());
-    return arr;
-  }
-
-  void Vendiendo(ArrayList<Producto> arr){
-    Venta V1;
-    int n,Id;
+  public void mostrarVoleta(ArrayList<Producto> productosComprados){
+    Venta v1;
+    int cant, id;
     Scanner tec;
     String fecha;
-    tec=new Scanner(System.in);
+
+    tec = new Scanner(System.in);
     System.out.println("Ingrese el ID");
-    Id=tec.nextInt();
+    id = tec.nextInt();
     System.out.println("Ingrese la fecha");
-    fecha=tec.nextLine();
-    V1=new Venta(Id,"Fecha");
-    for (int i=0;i<arr.size();i++) {
-      System.out.println("Ingrese cuantos "+ arr.get(i).nombre +"Va a comprar");
-      n=tec.nextInt();
-      V1.agregarDetalle(new DetalleVenta(arr.get(i), n));
+    fecha = tec.nextLine();
+    v1 = new Venta(id,"Fecha");
+    for (Producto p : productosComprados) {
+      System.out.println("Ingrese cuantos "+ p.nombre +" va a comprar");
+      cant = tec.nextInt();
+      v1.agregarDetalle(new DetalleVenta(p, cant));
     }
-    V1.mostrarTicket();
+    v1.mostrarTicket();
   }
 }
